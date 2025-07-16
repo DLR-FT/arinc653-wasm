761297474269686eaa16f8210c4f1fb085053e4b
wucke13 <wucke13+github@gmail.com>
2025-07-16 17:54:53 +0200

    feat: add second create_process example
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

---
+++ src/example_create_process_advanced.c
@@ -0,0 +1,60 @@
+#include "ARINC653-wasm.h"
+#include <stdio.h>
+
+// main periodic process
+void pp_main() {
+
+  // loop counter
+  APEX_UNSIGNED i;
+
+  while (1) {
+    // allocate buffer for message
+    APEX_BYTE msg_buf[256];
+
+    // write message to buffer
+    APEX_INTEGER msg_len = 0;
+    msg_len = snprintf((char *)msg_buf, sizeof(msg_buf), "hello #%lu", i);
+
+    // (error) return code
+    RETURN_CODE_TYPE err;
+
+    // report the message
+    REPORT_APPLICATION_MESSAGE(msg_buf, msg_len, &err);
+
+    // check if an error occured
+    if (err) {
+      ERROR_MESSAGE_TYPE msg_buf =
+          "caused an error during REPORT_APPLICATION_MESSAGE call";
+      RAISE_APPLICATION_ERROR(APPLICATION_ERROR, msg_buf, sizeof(msg_len),
+                              &err);
+    }
+
+    // increment the counter
+    i += 1;
+
+    // wait for next iteration
+    PERIODIC_WAIT(&err);
+  }
+}
+
+void warm_start() {
+  PROCESS_ATTRIBUTE_TYPE pa = {
+      .PERIOD = 0,
+      .TIME_CAPACITY = 0,
+      .ENTRY_POINT = &pp_main,
+      .STACK_SIZE = 0x10000, // 64 KiB
+      .BASE_PRIORITY = 0,
+      .DEADLINE = 0,
+      .NAME = "pp_main",
+  };
+  PROCESS_ID_TYPE pid;
+  RETURN_CODE_TYPE err;
+
+  CREATE_PROCESS(&pa, &pid, &err);
+}
+
+void cold_start() {
+  // do some cold-start-only intitialization business
+
+  warm_start();
+}
